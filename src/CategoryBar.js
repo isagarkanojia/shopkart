4d9adb86535ac44b161fb0a14cf2fe6535c72292
Sagar Kanojia <isagarkanojia@gmail.com>
2022-02-26 17:41:11 +0530

    code cleanup

--- src/CategoryBar.js
+++ src/CategoryBar.js
@@ -1,15 +1,44 @@
-const data = ["electronics", "jewelery", "men's clothing", "women's clothing"];
+import { useEffect, useState } from "react";
 
 function CategoryBar() {
-  return (
-    <div className="category-bar">
-      {data.map((categoryName) => (
-        <div key={categoryName} className="category-item">
-          {categoryName}
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [loadError, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((res) => {
+        setLoading(false);
+        setProducts(res);
+      })
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  function ui() {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    } else if (loadError) {
+      return <div>{loadError.message}</div>;
+    } else {
+      return (
+        <div className="category-items">
+          {!isLoading &&
+            products.map((categoryName) => (
+              <div key={categoryName} className="category-item">
+                {categoryName}
+              </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+      );
+    }
+  }
+
+  return <div className="category-bar">{ui()}</div>;
 }
 
 export default CategoryBar;
